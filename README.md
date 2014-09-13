c5615aea6007db31648da2e60a2f64ef94e1ea06
Osman Afridi <oafridi@atlassian.com>
2014-09-12 19:22:37 -0700

    clean site

--- README.md
+++
@@ -1,2 +0,0 @@
-oafridi.github.io
-=================
--- scripts/date.js
+++
@@ -1,9 +0,0 @@
-// window.onload = initDate;
-// function initDate() {
-//      var months = ['January','February','March','April','May','June','July',
-// 'August','September','October','November','December'];
-// var today = new Date();
-// today.setTime(today.getTime() );
-// document.getElementById("spanDate").innerHTML = months[today.getMonth()] + " " + today.getDate()+ ", " + today.getFullYear();
-// }
-
